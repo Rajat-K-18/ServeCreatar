f74271d69c4cf2ab61252472dd6f4404fb5ffd06
Rajat-K-18 <rj_rajat18@yahoo.in>
2017-03-27 23:49:06 +0530

    OpenCV integrated in the project

--- src/main/java/ServeCreatARMain.java
+++ src/main/java/ServeCreatARMain.java
@@ -1,14 +1,24 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.opencv.core.Scalar;
+
 
 public class ServeCreatARMain {
     private static final int MAX_MESSAGE_SIZE = 3000000;
     private static final int PORT_NO = 9999;
 
-    public static void main(String[] args) throws Exception {
-        startServer(); //do need to see what happens when multiple devices access at the same time
 
+    // Compulsory statement to run opencv
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+
+    public static void main(String[] args) throws Exception {
+
+
+        startServer(); //do need to see what happens when multiple devices access at the same time
     }
 
     private static void startServer() throws Exception {
